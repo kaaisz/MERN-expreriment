89aa6e6c95c0ffa28e1d3f66f838dd544bcd281c
Kaai Suzuki <suzydp@me.com>
2019-01-22 17:43:16 -0800

    Profile Field Validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile model
 const Profile = require('../../models/Profile');
 // Load User Profile
@@ -47,6 +50,14 @@ router.post(
   passport.authenticate('jwt', { session: false }), 
   (req, res) => {
 
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check Validation
+    if(!isValid) {
+      // return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
@@ -73,7 +84,7 @@ router.post(
     if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
-    profile.findOne({ user: req.user.id })
+    Profile.findOne({ user: req.user.id })
       .then(profile => {
         if(profile) {
           // update
@@ -95,7 +106,7 @@ router.post(
               }
 
               // Save profiles
-              new Profile(profileFields).save
+              new Profile(profileFields).save()
                 .then(profile => res.json(profile));
             })
         }
