7cccd5a0ae7cf7ab91ac8db1b16354f8912a1f71
Kaai Suzuki <suzydp@me.com>
2019-01-23 00:30:14 -0800

    Profile Field Validations - 2

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,18 +27,23 @@ router.get('/test', (req, res) => res.json({msg: "Profile Works"})); // output w
 // @route    GET api/profile
 // @desc     Get current users profile
 // @access   Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const errors = {};
+router.get(
+  '/', 
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    const errors = {};
 
-  Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      if(!profile) {
-        errors.noprofile = 'There is no profile for this user';
-        return res.status(404).json(errors);
-      }
-      res.json(profile)
-    })
-    .catch(err => res.status(404).json(err));
+    Profile.findOne({ user: req.user.id })
+      // we're able to populate fields from users into the response.
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if(!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile)
+      })
+      .catch(err => res.status(404).json(err));
 });
 
 
