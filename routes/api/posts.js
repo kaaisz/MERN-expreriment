5a321efba2cb1beee2853feea3d3c1070801bf09
Kaai Suzuki <suzydp@me.com>
2019-02-08 15:45:39 -0800

    Add & Remove comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -159,5 +159,56 @@ router.post(
     });
   }
 );
+
+
+
+// @route   POST api/posts/comment/:id
+// @desc    Add comment to post
+// @access  Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      // create comment object
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      }
+
+      // Add to comments array
+      post.comments.unshift(newComment);
+
+      // save
+      post.save().then(post => res.json(post))
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
+
+
+// @route   DELETE api/posts/comment/:id(post_id)/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      // check if comment exists
+      if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+        return res.status(404).json({ commentnotexist: 'Comment does not exist' })
+      }
+
+      // Get remove index
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+      // splice comment out of array
+      post.comments.splice(removeIndex, 1);
+      post.save().then(post => res.json(post))
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
 // export the router
 module.exports = router;
