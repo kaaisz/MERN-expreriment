32a6bdd698413eebde469fde1f09f2f973d770ad
Kaai Suzuki <suzydp@me.com>
2019-02-11 14:32:26 -0800

    Add Router to each components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+// because wanna use BrowserRouter as Router here 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import Footer from './components/layout/Footer';
 import './App.css';
 
+// A component placed router should be the kind of home page
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          {/* exact needs to add, otherwise multiple routes will invoke */}
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
